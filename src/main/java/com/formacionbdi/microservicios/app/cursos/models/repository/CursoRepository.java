5416d01adefdebb2058e4f85922c21f061a6ace9
Antonio Sanchez <antonio.san.87@hotmail.com>
2021-09-25 12:17:54 +0200

    Added first commit

---
+++ src/main/java/com/formacionbdi/microservicios/app/cursos/models/repository/CursoRepository.java
@@ -0,0 +1,8 @@
+package com.formacionbdi.microservicios.app.cursos.models.repository;
+
+import com.formacionbdi.microservicios.app.cursos.models.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+
+}
